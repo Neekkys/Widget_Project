6e74e10e9919ae759a7cc58ef979c6ec8a227a54
Nikita Kovetskiy <nikita,koveckij@gmail.com>
2025-11-16 21:25:21 +0300

    fixed module, update htmlcov

--- src/processing.py
+++ src/processing.py
@@ -22,28 +22,3 @@ def sort_by_date(operation_list: list[dict], reverse: bool = True) -> list[dict]
         return "Error, the list is empty"
     sorted_list_operation = sorted(operation_list, key=lambda x: x["date"], reverse=reverse)
     return sorted_list_operation
-
-
-if __name__ == "__main__":
-    print(
-        filter_by_state(
-            [
-                {"id": 41428829, "state": "EXECUTED", "date": "2019-07-03T18:35:29.512364"},
-                {"id": 939719570, "state": "EXECUTED", "date": "2018-06-30T02:08:58.425572"},
-                {"id": 594226727, "state": "CANCELED", "date": "2018-09-12T21:27:25.241689"},
-                {"id": 615064591, "state": "CANCELED", "date": "2018-10-14T08:21:33.419441"},
-            ],
-            # "CANCELED"
-        )
-    )
-
-    print(
-        sort_by_date(
-            [
-                {"id": 41428829, "state": "EXECUTED", "date": "2019-07-03T18:35:29.512364"},
-                {"id": 939719570, "state": "EXECUTED", "date": "2018-06-30T02:08:58.425572"},
-                {"id": 594226727, "state": "CANCELED", "date": "2018-09-12T21:27:25.241689"},
-                {"id": 615064591, "state": "CANCELED", "date": "2018-10-14T08:21:33.419441"},
-            ]
-        )
-    )
