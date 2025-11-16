2220b154ef7701f41eaf58ef15fb05a10fff7a0f
Nikita Kovetskiy <nkoveckij2014@yandex.ru>
2025-11-16 04:28:04 +0300

    Fixes processing.py, created test_processing.py(complete), fixes other module

--- src/processing.py
+++ src/processing.py
@@ -1,19 +1,25 @@
 import re
 
 
-def filter_by_state(operation_list: list[dict], state: str = "EXECUTED") -> list[dict]:
+def filter_by_state(operation_list: list[dict], state: str = "EXECUTED") -> list[dict] | str:
     """Функцуя ищет в списке словарей ключ 'state', сравнивает значение ключа с аргументом state
     и возвращает новый список словарей, в которых значение ключа равно аргументу"""
+    if not operation_list:
+        return "Error, the list is empty"
     operation_list_required = [
         operation for operation in operation_list if re.search(state, operation.get("state", ""))
     ]
+    if not operation_list_required:
+        return "Error, operation not found"
     # Сделано с рассчетом на то, что будет только 2 вида операций "EXECUTED" и "CANCELED"
     return operation_list_required
 
 
-def sort_by_date(operation_list: list[dict], reverse: bool = True) -> list[dict]:
+def sort_by_date(operation_list: list[dict], reverse: bool = True) -> list[dict] | str:
     """Функция, которая принимает список словарей, и сортирует их по значению
     ключа 'date', по умолчанию по убыванию"""
+    if not operation_list:
+        return "Error, the list is empty"
     sorted_list_operation = sorted(operation_list, key=lambda x: x["date"], reverse=reverse)
     return sorted_list_operation
 
--- tests/test_masks.py
+++ tests/test_masks.py
@@ -1,15 +1,18 @@
 import pytest
-from src.masks import get_mask_card_number, get_mask_account
+
+from src.masks import get_mask_account, get_mask_card_number
 
 
+# Функция get_mask_card_number
 def test_get_mask_card_number(card_number):
     assert get_mask_card_number(card_number) == "4111 11** **** 1111"
 
 
 def test_empty_get_mask_card_number():
-    assert get_mask_card_number('') == "Check your card number"
+    assert get_mask_card_number("") == "Check your card number"
 
 
+# Функция get_mask_account
 def test_get_mask_account(account_number):
     assert get_mask_account(account_number) == "**7890"
 
@@ -18,32 +21,39 @@ def test_empty_get_mask_account():
     assert get_mask_account("") == "Check your account number"
 
 
-@pytest.mark.parametrize("card_num, expected", [
-    ("5555555555554444", "5555 55** **** 4444"),
-    ("371449635398431", "Check your card number"),
-    ("6011-1111-1111-1117", "Check your card number"),
-    ("  3530111333300000  ", "3530 11** **** 0000"),
-    ("4111111111111111 ", "4111 11** **** 1111"),
-    ("510510510b105100", "Check your card number"),
-    ("30569309025904", "Check your card number"),
-    ("3566002020360505", "3566 00** **** 0505"),
-    ("6200000000000005", "6200 00** **** 0005"),
-    ("1234567812345670", "1234 56** **** 5670"),
-])
+# Параметризация обоих функций
+@pytest.mark.parametrize(
+    "card_num, expected",
+    [
+        ("5555555555554444", "5555 55** **** 4444"),
+        ("371449635398431", "Check your card number"),
+        ("6011-1111-1111-1117", "Check your card number"),
+        ("  3530111333300000  ", "3530 11** **** 0000"),
+        ("4111111111111111 ", "4111 11** **** 1111"),
+        ("510510510b105100", "Check your card number"),
+        ("30569309025904", "Check your card number"),
+        ("3566002020360505", "3566 00** **** 0505"),
+        ("6200000000000005", "6200 00** **** 0005"),
+        ("1234567812345670", "1234 56** **** 5670"),
+    ],
+)
 def test_get_mask_card_number_param(card_num, expected):
     assert get_mask_card_number(card_num) == expected
 
 
-@pytest.mark.parametrize("account_num, expected", [
-    ("12345678901234567890", "**7890"),
-    ("00000000000000000000", "**0000"),
-    ("99999999999999999999", "**9999"),
-    ("11111111111111111111", "**1111"),
-    ("55555555555555555555", "**5555"),
-    ("  22222222222222222222  ", "**2222"),
-    ("5555555555555555555", "Check your account number"),
-    ("123123123123123123123123123123", "Check your account number"),
-    ("1111111111111111111h", "Check your account number")
-])
+@pytest.mark.parametrize(
+    "account_num, expected",
+    [
+        ("12345678901234567890", "**7890"),
+        ("00000000000000000000", "**0000"),
+        ("99999999999999999999", "**9999"),
+        ("11111111111111111111", "**1111"),
+        ("55555555555555555555", "**5555"),
+        ("  22222222222222222222  ", "**2222"),
+        ("5555555555555555555", "Check your account number"),
+        ("123123123123123123123123123123", "Check your account number"),
+        ("1111111111111111111h", "Check your account number"),
+    ],
+)
 def test_get_mask_account_param(account_num, expected):
     assert get_mask_account(account_num) == expected
