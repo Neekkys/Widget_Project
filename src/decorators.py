f233f3bcb45d1ed26b348451582fa504821f313f
Nikita Kovetskiy <nkoveckij2014@yandex.ru>
2025-11-30 02:07:04 +0300

    new module decorators.py(complited)

---
+++ src/decorators.py
@@ -0,0 +1,34 @@
+from functools import wraps
+
+from datetime import datetime
+
+from src.config import BASE_DIR
+
+
+def log(filename=None):
+    """ Декоратор, который записывает в указанный файл логи функции """
+    def _log(func):
+        @wraps(func)
+        def wrapper(*args, **kwargs):
+            # Время вызова, имя функции
+            call_time = datetime.now().strftime("%d.%m.%Y %H:%M:%S")
+            func_name = func.__name__
+            try:
+                result_func = func(*args, **kwargs)
+                log_message = (
+                    f"{call_time} - {func_name} - ok\n"
+                )
+            except(Exception) as e:
+                log_message = (
+                    f"{call_time} - {func_name} error: {type(e).__name__} - {str(e)}. Inputs: {args}, {kwargs}\n"
+                )
+                result_func = None
+            if filename:
+                data_dir = BASE_DIR.joinpath("data", filename)
+                with open(data_dir, "a", encoding="UTF-8") as f:
+                    f.write(log_message)
+            else:
+                print(log_message)
+            return result_func
+        return wrapper
+    return _log
